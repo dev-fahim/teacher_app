30276c032123930a4f6c642b12c5db8b839873b4
dev-fahim <fahim6668@gmail.com>
2018-11-20 13:29:23 +0600

    Updated some angular stuff

--- myproject/views.py
+++ myproject/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def index(request):
+def index(request, *args, **kwargs):
     return render(request, 'index.html', {})
 
 
