3e82e005a04754870d9b52ed1a001ce993602725
dev-fahim <fahim6668@gmail.com>
2018-12-26 01:47:14 +0600

    Moved owner related files to owners app

--- owners/cashiers/api/urls.py
+++ owners/cashiers/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
-from owners.cashiers.api.views import CashierAPIView, CashierCreateAPIView, CashierListApiView
+from owners.cashiers.api.views import CashierCreateAPIView, CashierListApiView
 
 app_name = 'api_cashier'
 
 urlpatterns = [
-    path('cashier/', include([
-        path('', CashierAPIView.as_view()),
-        path('all/', CashierListApiView.as_view()),
+    path('owner/cashiers/', include([
+        path('', CashierListApiView.as_view()),
         path('create/', CashierCreateAPIView.as_view()),
     ])),
 ]
