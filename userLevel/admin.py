280ecd9da44ace8b29c205586dc39d057b171fc4
Fahim Al Islam <34773501+dev-fahim@users.noreply.github.com>
2019-01-03 00:30:26 +0600

    Added user level feature

---
+++ userLevel/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from userLevel.models import UserLevelModel
+# Register your models here.
+
+admin.site.register(UserLevelModel)
---
+++ userLevel/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class UserLevelModel(models.Model):
+    LEVEL_CHOICES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5)
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_level')
+    level = models.IntegerField(choices=LEVEL_CHOICES)
+
+    def __str__(self):
+        return self.user
